7954b5109dce4a3cfeb1ece869e0b176bded0110
Tim254 <tim.mailu@gmail.com>
2022-06-28 23:44:28 +0300

    Done.

--- main.js
+++ main.js
@@ -3,8 +3,40 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+//document.addEventListener('DOMContentLoaded', ()=>{
 
+ const modal= document.getElementById('modal');
+ const hearts = document.querySelectorAll(".like-glyph");
+ //add class hidden to modal div
+ modal.className = "hidden";
+  
+  
+  const likePost = (event) =>{
 
+    const heart = event.target
+    heart.innerText = EMPTY_HEART
+    mimicServerCall()
+    //if server call is successful, change heart to full heart
+      .then( responce => {
+        (heart.innerText === EMPTY_HEART)? 
+        (heart.innerText = FULL_HEART, heart.classList.add("activated-heart"))
+        :(heart.innerText = EMPTY_HEART, heart.classList.remove("activated-heart")) 
+     
+      })
+      //if server call fails, display error message
+      .catch(error => {
+        modal.classList.remove("hidden");
+        modal.querySelector("#modal-message").textContent = error;
+        setTimeout( () => {
+          modal.classList.add("hidden")
+        }, 3000)
+      }) 
+    
+  }
+  //call event for each love icons
+  hearts.forEach( heart => {
+    heart.addEventListener("click", likePost)
+  })
 
 
 //------------------------------------------------------------------------------
